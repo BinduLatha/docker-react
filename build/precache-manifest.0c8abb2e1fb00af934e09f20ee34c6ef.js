8cdcc34ac4e914c03d146eb9e4daaae5722540b3
Bindu Latha Nuthalapati <bnuthalapati@gemini-us.com>
2019-12-17 11:54:44 +0530

    Initial Commit

---
+++ build/precache-manifest.0c8abb2e1fb00af934e09f20ee34c6ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52747a1d3192c75af7cb595b936eff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b6a7e8fc9e7a7025d1d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c8fc80d23a551aa72b72",
+    "url": "/static/js/2.fc6ca83f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fc6ca83f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b6a7e8fc9e7a7025d1d",
+    "url": "/static/js/main.621c5d97.chunk.js"
+  },
+  {
+    "revision": "6e717951369aa5bd5963",
+    "url": "/static/js/runtime-main.da9b9a57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
